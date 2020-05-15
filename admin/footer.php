c835d058cc3a235fb86d4b1326eeb077f2131bbf
=sieuthienha7755 <=sieuthienha7755@gmail.com>
2020-05-15 18:41:13 +0700

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    <div class="col-sm-6">
+                        Copyright &copy; 2018 Ela Admin
+                    </div>
+                    <div class="col-sm-5 text-right" >
+                        Designed by NCKH2020
+                    </div>
+                </div>
+            </div>
+        </footer>
